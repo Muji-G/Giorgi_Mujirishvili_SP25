8a4df2d27de64e46abcca21aed3312d4bfb096d4
Muji-G <georgemujiri01@gmail.com>
2025-05-25 16:32:06 +0400

    Updated depending on reviews
    .

--- SQL_Analysis_Giorgi_Mujirishvili_WindFrames_HW.sql
+++ SQL_Analysis_Giorgi_Mujirishvili_WindFrames_HW.sql
@@ -42,31 +42,32 @@ ORDER BY sr.country_region,sr.calendar_year,sr.channel_desc;
 -- Task 2
 
 
-SELECT t.calendar_week_number,
-       t.time_id,
-       t.day_name,
-       SUM(s.amount_sold) AS sales,
-       SUM(SUM(s.amount_sold)) OVER (PARTITION BY t.calendar_week_number ORDER BY t.calendar_week_number, t.time_id RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cum_sum,
-       ROUND(CASE
-                WHEN LOWER(t.day_name) IN ('tuesday', 'wednesday', 'thursday', 'saturday', 'sunday') 
-                THEN AVG(SUM(s.amount_sold)) OVER (ORDER BY t.time_id ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING)
-                WHEN LOWER(t.day_name) = 'monday' 
-                THEN AVG(SUM(s.amount_sold)) OVER (ORDER BY t.time_id ROWS BETWEEN 2 PRECEDING AND 1 FOLLOWING)
-                WHEN LOWER(t.day_name) = 'friday' 
-                THEN AVG(SUM(s.amount_sold)) OVER (ORDER BY t.time_id ROWS BETWEEN 1 PRECEDING AND 2 FOLLOWING)
-        END, 2) AS centered_3_day_avg 
-FROM sh.times t 
-JOIN sh.sales s ON s.time_id = t.time_id
-WHERE t.calendar_week_number BETWEEN 49 AND 51
-AND t.calendar_year = 1999
-GROUP BY t.calendar_week_number,
-         t.time_id,
-         t.day_name
-ORDER BY t.calendar_week_number, t.time_id;
-
--- In this caseRANGE is used to sum all sales from the start of the week up until the current day and
--- ROWS is used to average sales from the exact previous and next days. This ensures the average is centered around each day.
-
+WITH sales_report AS 
+	(SELECT 
+		t.calendar_week_number,
+		t.time_id,
+		t.day_name,
+		SUM(s.amount_sold) AS sales,
+		SUM(SUM(s.amount_sold)) OVER (PARTITION BY t.calendar_week_number,t.calendar_year 
+			order by t.time_id RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cum_sum,
+		ROUND(case 
+			WHEN LOWER(t.day_name) = 'monday' then
+				AVG(SUM(s.amount_sold)) OVER (ORDER  BY t.time_id ROWS BETWEEN 2 PRECEDING AND 1 FOLLOWING)
+			WHEN LOWER(t.day_name) = 'friday' THEN
+				AVG(SUM(s.amount_sold)) OVER (ORDER  BY t.time_id ROWS BETWEEN 1 PRECEDING AND 2 FOLLOWING)
+			ELSE 
+				AVG(SUM(s.amount_sold)) OVER (ORDER  BY t.time_id ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) END,2) AS centered_3_day_avg
+	FROM sh.sales s
+	INNER JOIN sh.times t ON t.time_id = s.time_id
+	WHERE t.calendar_week_number BETWEEN 48 AND 52
+	AND t.calendar_year = 1999
+	GROUP BY t.calendar_week_number,t.time_id,t.day_name
+)
+SELECT
+	sr.*
+FROM sales_report sr
+WHERE sr.calendar_week_number BETWEEN 49 AND 51
+ORDER BY sr.time_id;
 
 
 -- Task 3
@@ -79,12 +80,11 @@ SELECT c.country_region,
        ch.channel_desc,
 	   SUM(s.amount_sold) AS amount_sold,
        SUM(SUM(s.amount_sold)) OVER (PARTITION BY t.calendar_year, c.country_region ORDER BY t.calendar_week_number RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_sales,
-
-       -- centered 3-day moving average using rows (previous, current, and following days)
+-- centered 3-day moving average using rows (previous, current, and following days)
 
 ROUND(AVG(SUM(s.amount_sold)) OVER (PARTITION BY c.country_region, t.calendar_year, ch.channel_desc ORDER BY t.time_id ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING), 2) AS centered_3_day_avg,
 
-		-- ranking channels within each region using groups
+-- ranking channels within each region using groups
 
 RANK() OVER (PARTITION BY c.country_region ORDER BY SUM(s.amount_sold) DESC GROUPS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS channel_rank
 FROM sh.countries c
@@ -92,7 +92,7 @@ JOIN sh.customers cu ON c.country_id = cu.country_id
 JOIN sh.sales s ON cu.cust_id = s.cust_id            
 JOIN sh.channels ch ON ch.channel_id = s.channel_id  
 JOIN sh.times t ON s.time_id = t.time_id             
-WHERE c.country_region IN ('Americas', 'Asia', 'Europe')
+WHERE c.country_region IN LOWER('americas', 'asia', 'europe')
 AND t.calendar_year BETWEEN 1999 AND 2001
 GROUP BY c.country_region, t.calendar_year, ch.channel_desc, t.calendar_week_number, t.time_id
 )
